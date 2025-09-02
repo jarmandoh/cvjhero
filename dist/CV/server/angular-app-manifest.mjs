39f43f54cdaf62e3abc669f66a93e4f4e65f8e66
janier hernandez <janier007@gmail.com>
2025-09-02 09:03:49 -0500

    Eliminar archivos vacíos y optimizar la estructura del proyecto

--- dist/CV/server/angular-app-manifest.mjs
+++ dist/CV/server/angular-app-manifest.mjs
@@ -33,13 +33,13 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 18060, hash: '54e4d08bdd738fb49958d0cf78addf5a45ed7890278856fb6989a8e5f05c89fd', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1091, hash: '4c8d4386187e24f0471c777997f2e4a2300bbb6e74f568ecd656be196670a3d0', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'contacto/index.html': {size: 31164, hash: '5a17b2bdd9bfa8e0bf598f1804b5a3f604c3fc5e1ef60801e10003ab61d9e95f', text: () => import('./assets-chunks/contacto_index_html.mjs').then(m => m.default)},
-    'proyectos/index.html': {size: 31842, hash: '18a7a408d7e216a312aabc2fffeccb8a0615ba6b0b474b24a40aa1d3bb2164ea', text: () => import('./assets-chunks/proyectos_index_html.mjs').then(m => m.default)},
-    'index.html': {size: 30679, hash: '3326f774df81e7db6c8f57ce4ac6cb87095081e1b8fb108723d4aa8e9802e219', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
-    'experiencia/index.html': {size: 48108, hash: '5ae079107f5b7499eef9a67718f247f03be01d1c90da3e24e0e1c6d3ba08a4b8', text: () => import('./assets-chunks/experiencia_index_html.mjs').then(m => m.default)},
-    'servicios/index.html': {size: 31692, hash: '87689152ea5f305294cadd9080fbc2aae217b6eb8f8da58a938eefe3f0f7b516', text: () => import('./assets-chunks/servicios_index_html.mjs').then(m => m.default)},
-    'styles-67LQAMSA.css': {size: 31076, hash: 'ApMLXCp47cg', text: () => import('./assets-chunks/styles-67LQAMSA_css.mjs').then(m => m.default)}
+    'index.csr.html': {size: 17985, hash: 'ec01d4bd44f45460c880f03ee405cce958b43c48cb064076318ed27660a8e740', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1091, hash: '92c9c2b0fb5a35d15b14e7a86934c6a93f3e9a4c15b42f4cb33ed80d88e5eac0', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'contacto/index.html': {size: 31084, hash: '686fe557aecb5b94ad2a2e602f963a44ff8597eb3690aed623006746ee048d31', text: () => import('./assets-chunks/contacto_index_html.mjs').then(m => m.default)},
+    'servicios/index.html': {size: 31612, hash: '29fde2cb6ac069887422a300817b9a24b2753addd9bb27c09ee014f2deb23e31', text: () => import('./assets-chunks/servicios_index_html.mjs').then(m => m.default)},
+    'index.html': {size: 30599, hash: 'e48351e2f037d77aacacf71e80e52b836726d26b8ae63ba782e92821d88e32f5', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'proyectos/index.html': {size: 31762, hash: '8ebf52094d5cccd1775139bb474dd3d5bbb348834f574b37df84430774ba7dc8', text: () => import('./assets-chunks/proyectos_index_html.mjs').then(m => m.default)},
+    'experiencia/index.html': {size: 48028, hash: '763ed524a415a42cb260137d86514389fad38bc2c9ca1980f82f53cf16ebdf01', text: () => import('./assets-chunks/experiencia_index_html.mjs').then(m => m.default)},
+    'styles-JCKWOO4X.css': {size: 30205, hash: 'QsqnTSdOZuY', text: () => import('./assets-chunks/styles-JCKWOO4X_css.mjs').then(m => m.default)}
   },
 };
